270e70753ff1a5e36dfecf5b09548afd447dfe3f
JanainaSantosSilva <janaina.santos231@hotmail.com>
2022-09-28 14:55:19 -0300

    vue upgrade fro vue3

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,10 +1,7 @@
-import Vue from "vue";
+import { createApp } from "vue";
 import App from "./main.vue";
 
 // import WebCam from "../../src";
 // Vue.use(WebCam);
 
-new Vue({
-  el: "#app",
-  render: h => h(App)
-});
+createApp({}).mount("#app");
